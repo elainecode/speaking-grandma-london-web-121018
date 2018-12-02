557d404048cb0004a51fd44b2d801c72cb90d17c
I_AM Elaine <elaine.onb@gmail.com>
2018-12-02 15:14:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,15 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(str)
+  if str = str.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif str == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!" 
+  else 
+    return "NO, NOT SINCE 1938!"
+  end
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
